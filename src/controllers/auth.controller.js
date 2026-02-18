5d0d3f11ec99ee9a4431540d82ce761a6e548b42
Akbar <akbar.code20@gmail.com>
2026-02-18 08:09:53 +0530

    applied Contrainst & checks

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,13 @@ const jwt=require("jsonwebtoken");
 async function registerUser(req,res){
     const {username,email,password}=req.body;
 
+    const isUserAlreadyExists= await userModel.findOne({username});
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message:"Username already exist"
+        })
+    }
     const user= await userModel.create({
         username,email,password
     })
@@ -11,11 +18,12 @@ async function registerUser(req,res){
     // creating token
     const token=jwt.sign(// user's data & it should be unique
         {id :user._id},process.env.JWT_SECRET)
+    
+    res.cookie("token",token);
 
     res.status(201).json({
         message: "User registered Successfully",
         user,
-        token
     })
 }
 
